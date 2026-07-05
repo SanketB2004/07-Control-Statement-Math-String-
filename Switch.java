b6b84ea7be89c26269b2130e3eaf4e504c07765a
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-05 09:08:56 +0000

    Commit

---
+++ Switch.java
@@ -0,0 +1,40 @@
+/**
+ * Switch
+ */
+public class Switch {
+
+    public static void main(String[] args) {
+        
+      int a = 0;
+
+      switch (a) {
+        case 1:
+            System.out.println("Mon");
+            break;
+            case 2:
+            System.out.println("tue");
+            break;
+            case 3:
+            System.out.println("wend");
+            break;
+            case 4:
+            System.out.println("thi");
+            break;
+            case 5:
+            System.out.println("fri");
+            break;
+            case 6:
+            System.out.println("satur");
+            break;
+            case 7:
+            System.out.println("saunday");
+            break;
+            
+      
+        default:System.out.println("Hanuman Day");
+            break;
+      }
+         
+        
+    }
+}
