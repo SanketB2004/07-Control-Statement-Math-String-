3efaba3fda12aaee307c5de3982d649adfe44395
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-07 09:05:18 +0000

    Commit

---
+++ challenges/code_detector.java
@@ -0,0 +1,11 @@
+package challenges;
+
+public class code_detector {
+    public static void main(String[] args) {
+        
+        int mark = 50 ; 
+
+        String ternary = mark >= 80 ? "High" : (mark >= 50 ? "mordaret": "low");
+System.out.println(ternary);
+    }
+}
