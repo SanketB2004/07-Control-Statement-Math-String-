945032b8a67e962feea866b84da06a1171453de2
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-07 08:38:44 +0000

    Commit

---
+++ challenges/min.java
@@ -0,0 +1,15 @@
+package challenges;
+
+public class min {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 5;
+
+        int c = a < b ? a : b;
+
+        System.out.println(c);
+    }
+
+}
