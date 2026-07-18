77f3f99b35093d582d9c2441c69d9f96384ffc6e
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-18 11:34:57 +0000

    Commit

---
+++ challenges_02/do_while02.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * do_while02
+ */
+public class do_while02 {
+
+    // number gassing game used  to do while number 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = 5 ,  guess ;
+
+        System.out.println("Welcome to number gassing game");
+
+        do {
+            System.out.println("Please gassing number 0 to 10");
+        guess = sc.nextInt();
+
+            
+        } while (num!= guess);
+
+        System.out.println("You have wins");
+
+
+    }
+}
